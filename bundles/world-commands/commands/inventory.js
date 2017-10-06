20f1e82b6529610d9914619042fc031a7eec791f
Miao (Wilson) Liu <wilson@clicktocloud.com>
2017-10-06 22:07:44 +1100

    add completion to command inventory

--- bundles/world-commands/commands/inventory.js
+++ bundles/world-commands/commands/inventory.js
@@ -6,6 +6,7 @@ module.exports = (srcPath, bundlePath) => {
 
   return {
     usage: 'inventory',
+    options: {},
     command : (state) => (args, player) => {
       if (!player.inventory || !player.inventory.size) {
         return Broadcast.sayAt(player, "You aren't carrying anything.");

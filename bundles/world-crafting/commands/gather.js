7779bcb66cc8e92e67f2f3e236a3ef432ac1761a
Miao (Wilson) Liu <wilson@clicktocloud.com>
2017-10-07 14:39:11 +1100

    add world crafting extension

---
+++ bundles/world-crafting/commands/gather.js
@@ -0,0 +1,58 @@
+'use strict';
+
+module.exports = (srcPath, bundlePath) => {
+  const B = require(srcPath + 'Broadcast');
+  const { CommandParser } = require(srcPath + 'CommandParser');
+  const Random = require(srcPath + 'RandomUtil');
+  const Crafting = require(bundlePath + 'world-crafting/lib/Crafting');
+  const ItemUtil = require(bundlePath + 'world-lib/lib/ItemUtil');
+  const SearchUtil = require(bundlePath + 'world-lib/lib/SearchUtil');
+
+  return {
+    usage: "gather <item>",
+    options: (state, player) => {
+      let options = {};
+      SearchUtil.listKeywordsOfRoomItems(player, item => item.getBehavior('resource'))
+          .forEach(keyword => options[keyword] = {});
+
+      return options;
+    },
+    command: state => (args, player) => {
+      if (!args || !args.length) {
+        return B.sayAt(player, "Gather what?");
+      }
+
+      let node = CommandParser.parseDot(args, player.room.items);
+
+      if (!node) {
+        return B.sayAt(player, "You don't see anything like that here.");
+      }
+
+      const resource = node.getBehavior('resource');
+      if (!resource) {
+        return B.sayAt(player, "You can't gather anything from that.");
+      }
+
+      if (!player.getMeta('resources')) {
+        player.setMeta('resources', {});
+      }
+
+      let result = [];
+      for (const material in resource.materials) {
+        const entry = resource.materials[material];
+        const amount = Random.inRange(entry.min, entry.max);
+        if (amount) {
+          const resItem = Crafting.getResourceItem(material);
+          const metaKey = `resources.${material}`;
+          player.setMeta(metaKey, (player.getMeta(metaKey) || 0) + amount);
+          B.sayAt(player, `<green>You gather: ${ItemUtil.display(resItem)} x${amount}.`);
+        }
+      }
+
+      // destroy node, will be respawned
+      state.ItemManager.remove(node);
+      B.sayAt(player, `${ItemUtil.display(node)} ${resource.depletedMessage}`);
+      node = null;
+    }
+  };
+};
